b86279bfba1f728cd5ab657b6eb4e6117ea3fe19
01eral <01eral.nti@gmail.com>
2019-02-28 12:58:24 +0100

    Update Gissa_tal_spel.java

--- src/gissaspel/Gissa_tal_spel.java
+++ src/gissaspel/Gissa_tal_spel.java
@@ -12,16 +12,30 @@ public class Gissa_tal_spel {
 		
 		while (restart) {
 		
-		int answer = (int) (Math.random() * 10);
+		int answer = (int) (Math.random() * 100);
+		System.out.println("Guess the Number");
 
 		while (!guess) {
+			
 			int key = keyboard.nextInt();
+			
+			
+			
 			if (answer == key) {
 				System.out.println("Right guess");
 				guess = true;
 				restart = false;
 			} else {
+				
+				if (key > answer) {
+					System.out.println("lower");
+				}
+				else {
+					System.out.println("higher");
+				}
+				
 				System.out.println("Try again");
+				
 			}
 
 		}
