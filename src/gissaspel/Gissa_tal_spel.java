c032247074fc7d57c4b4f2fbbb37f37163e6ac70
01eral <01eral.nti@gmail.com>
2019-03-22 14:10:58 +0100

    Update Gissa_tal_spel.java

--- src/gissaspel/Gissa_tal_spel.java
+++ src/gissaspel/Gissa_tal_spel.java
@@ -15,6 +15,8 @@ public class Gissa_tal_spel {
 		int answer = (int) (Math.random() * 100);
 		System.out.println("Guess the Number"); 
 
+		
+		
 		while (!guess) {
 			
 			int key = keyboard.nextInt();
@@ -42,7 +44,7 @@ public class Gissa_tal_spel {
 		System.out.println("Do you want to Restart? 1 = yes, 0 = no"); 
 		
 		int startover = keyboard.nextInt();
-		
+		 
 		if (startover == 0) {
 			restart = false;
 			
